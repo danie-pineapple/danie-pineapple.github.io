37c7f2e1c11cc7f1be6fcff32558f5aa02b21e6b
Isaac Ruiz Godínez <isaac@macbooks-MacBook-Air.local>
2026-06-18 13:19:58 +0200

    fix base path tras renombrar repo

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/danielapineapple/',
+  base: '/daniela_pineaple/',
   plugins: [react()],
 })
