016b5c373ffb49e0e27dabda783bdf7d7fa13805
Isaac Ruiz Godínez <isaac@macbooks-MacBook-Air.local>
2026-06-18 13:51:45 +0200

    setup inicial portafolio Daniela

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/daniela_pineaple/',
+  base: '/daniela-pineapple/',
   plugins: [react()],
 })
